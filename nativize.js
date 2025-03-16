50dbb32be504bd365b7942354edbc8fe34bde7c0
Garden Gim <50333551+gardengim@users.noreply.github.com>
2025-03-16 22:28:02 +0900

    feat: unsuccessful command handling

--- nativize.js
+++ nativize.js
@@ -4,25 +4,56 @@ export const check = async () => {
 };
 export const prepare = async () => {
 };
+
 export const build = async ({ url }) => {
   //setup builddir
   await new Deno.Command("meson", {
     args: ["setup", "--reconfigure", "builddir", "-Dnativize_url=" + url],
     cwd: import.meta.dirname,
-  }).spawn().status;
+  })
+    .spawn()
+    .status
+    .then(({ success }) => {
+      if (!success) {
+        throw Error(`meson setup --reconfigure builddir -Dnativize_url=${url}`);
+      }
+    });
+
   //compile
   await new Deno.Command("meson", {
     args: ["compile", "-C", "builddir"],
     cwd: import.meta.dirname,
-  }).spawn().status;
+  })
+    .spawn()
+    .status
+    .then(({ success }) => {
+      if (!success) {
+        throw Error(`meson compile -C builddir`);
+      }
+    });
 };
+
 export const run = async () => {
-  await new Deno.Command(`${import.meta.dirname}/builddir/nativize`).spawn()
-    .status;
+  await new Deno.Command(`${import.meta.dirname}/builddir/nativize`)
+    .spawn()
+    .status
+    .then(({ success }) => {
+      if (!success) {
+        throw Error(`${import.meta.dirname}/builddir/nativize`);
+      }
+    });
 };
+
 export const clean = async () => {
   await new Deno.Command("meson", {
     args: ["compile", "--clean", "-C", "builddir"],
     cwd: import.meta.dirname,
-  }).spawn().status;
+  })
+    .spawn()
+    .status
+    .then(({ success }) => {
+      if (!success) {
+        throw Error(`meson compile --clean -C builddir`);
+      }
+    });
 };
